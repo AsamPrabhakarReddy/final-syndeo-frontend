65522a32451db5180969354e7f4ce83a5fbb436c
AsamPrabhakarReddy <94506736+AsamPrabhakarReddy@users.noreply.github.com>
2024-09-20 10:06:15 +0530

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -20,11 +20,11 @@ function Calendar() {
     }
   return (
     <div className="h-screen"> 
-      <div className="w-full bg-white shadow-xl sticky fixed ">
+{/*       <div className="w-full bg-white shadow-xl sticky fixed ">
         <h1 className="max-w-[1400px] text-center text-2xl h-[75px] items-center font-bold p-5 mx-auto">
           Importing Calendar
         </h1>
-      </div>
+      </div> */}
       <div  className={`max-w-[1400px] mx-auto h-[calc(100vh-75px)] mt-4 mb-10 ${
           showModel ? "opacity-50 pointer-events-none" : "opacity-100"
         }`}>
